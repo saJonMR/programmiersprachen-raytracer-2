e4e138d6f2001ddf2278f83fbd8f3f103109ca7d
Jonas Roquette <roquettejonas@gmail.com>
2020-07-17 13:50:35 +0200

    added Sphere testsw

--- source/tests.cpp
+++ source/tests.cpp
@@ -6,7 +6,7 @@
 
 TEST_CASE("Box constructor, area and volume test", "[box]") {
   Box one{{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}};
-  Box two{{0.0, 0.0, 0.0}, {1.0, 1.0, 1.0}};
+  Box two;
   REQUIRE(one.area() == 0);
   REQUIRE(one.volume() == 0);
   REQUIRE(two.area() == 6);
@@ -14,7 +14,12 @@ TEST_CASE("Box constructor, area and volume test", "[box]") {
 }
 
 TEST_CASE("Sphere constructor, area and volume test", "[sphere]") {
-  
+  Sphere one;
+  Sphere two{{0.0, 0.0, 0.0}, 3.5};
+  REQUIRE(one.area() == Approx(12.5663706));
+  REQUIRE(one.volume() == Approx(4.1887902));
+  REQUIRE(two.area() == Approx(153.93804));
+  REQUIRE(two.volume() == Approx(179.59438));
 }
 
 int main(int argc, char *argv[])
